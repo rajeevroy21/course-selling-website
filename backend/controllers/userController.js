45cfb0821e3fd9db8471a7c60ee723aa681e7b46
Rajeev kumar <rajeevroy70701@gmail.com>
2025-07-22 12:28:52 +0530

    update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ import Course from "../models/Course.js"
 import { CourseProgress } from "../models/CourseProgess.js"
 export const getUserData = async(req,res)=>{
     try {
-        const userId = "user_2xgpzpbfybiTOZsPnL7DMpyOPEi"
+        const userId = req.auth.userId
         const user = await User.findById(userId)
         if(!user){
             return res.json({ success: false, message: 'User Not Found'})
@@ -33,7 +33,7 @@ export const purchaseCourse = async (req,res)=>{
     try {
         const { courseId } = req.body
         const {origin} = req.headers
-        const userId= "user_2xgpzpbfybiTOZsPnL7DMpyOPEi"
+         const userId = req.auth.userId
         const userData = await User.findById(userId)
         const courseData = await Course.findById(courseId)
 
@@ -79,7 +79,7 @@ export const purchaseCourse = async (req,res)=>{
 
 export const updateUserCourseProgress = async(req,res)=>{
     try {
-        const userId = "user_2xgpzpbfybiTOZsPnL7DMpyOPEi"
+         const userId = req.auth.userId
         const {courseId, lectureId} = req.body
 
         const progressData = await CourseProgress.findOne({userId, courseId})
@@ -107,7 +107,7 @@ export const updateUserCourseProgress = async(req,res)=>{
 
 export const getUserCourseProgress = async(req,res)=>{
     try {
-         const userId = "user_2xgpzpbfybiTOZsPnL7DMpyOPEi"
+          const userId = req.auth.userId
          const { courseId } = req.body
 
          const progressData = await CourseProgress.findOne({userId, courseId})
@@ -121,7 +121,7 @@ export const getUserCourseProgress = async(req,res)=>{
 //Add User Rating to course
 
 export const addUserRating = async (req, res)=>{
-    const userId="user_2xgpzpbfybiTOZsPnL7DMpyOPEi"
+     const userId = req.auth.userId
     const {courseId, rating} = req.body;
     if(!courseId || !userId || !rating || rating <1 || rating > 5){
          return res.json({success: false, message: 'Invalid Details'});
